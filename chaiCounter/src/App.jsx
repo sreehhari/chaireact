e8e4c47307abb911508af5b546aa32a4d6d2ae8c
Sreehari Sujith <sreehari9300@gmail.com>
2024-06-30 12:01:40 +0530

    added hooks

--- chaiCounter/src/App.jsx
+++ chaiCounter/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let value =10
+  let [value,setValue]=useState(10);
+  let add =()=>{
+    value = value+1
+    setValue(value)
+    console.log("clicked",value)
+  }
+  let minus=()=>{
+    value=value-1
+    setValue(value)
+    console.log("clicked",value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Chai counter</h1>
+     <h3>Counter value : {value}</h3>
+     <button onClick={add}>Add Value</button>
+     <br />
+     <button onClick={minus}>Reduce Value</button>
     </>
   )
 }
