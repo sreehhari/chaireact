22f1b35f07a8f76b84b7a65f138385eb732da53e
Sreehari Sujith <sreehari9300@gmail.com>
2024-06-30 12:07:02 +0530

    added value limiter

--- chaiCounter/src/App.jsx
+++ chaiCounter/src/App.jsx
@@ -4,14 +4,25 @@ function App() {
   // let value =10
   let [value,setValue]=useState(10);
   let add =()=>{
+    if(value!=20){
     value = value+1
     setValue(value)
+
     console.log("clicked",value)
+    }
+    else{
+      setValue(value)
+    }
   }
   let minus=()=>{
+    if(value!=0){
     value=value-1
     setValue(value)
     console.log("clicked",value)
+    }
+    else{
+      setValue(value)
+    }
   }
 
   return (
